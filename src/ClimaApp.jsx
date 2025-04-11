6bcabe10eaa582cea9f82483bd243c46bd530562
Guillermo Rosas <guillermorosas@tecnomotum.com>
2025-04-11 12:28:44 -0600

    se agrega Estilos Bulma

--- src/ClimaApp.jsx
+++ src/ClimaApp.jsx
@@ -4,7 +4,7 @@ export const ClimaApp = () => {
 
     return(
         <div>
-            <h1> Gordo </h1>
+            <h2 className="title is-2 has-text-centered"> Gordo </h2>
         </div>
     );
 }

5093c737734f6a4f0c43a7ed5b5d553d5fa721ba
Laptop <memorosas.00@gmail.com>
2025-04-10 22:03:24 -0600

    Segundo commit, se agrega domi page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {wheaterApp} from './wheaterApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <wheaterApp />
   </StrictMode>,
 )
---
+++ src/wheaterApp.jsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+export default function wheaterApp() {
+  return (
+    <div>wheaterApp</div>
+  )
+}

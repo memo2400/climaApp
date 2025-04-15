3e12bf9ee4e2d0759d58cb8d0c07755b8977bbb5
Guillermo Rosas <guillermorosas@tecnomotum.com>
2025-04-15 12:43:41 -0600

    se agrega footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ClimaApp } from './ClimaApp'
 import { NavBar } from './basePage/navBar'
+import { Footer } from './basePage/Footer'
 
 // import {wheaterApp} from './wheaterApp' // asi no sirve debe iniciarf ocn mayusculas
 // import {appClima} from './WheaterApp'
+
 
 createRoot(document.getElementById('root')).render(
 // ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,5 +15,6 @@ createRoot(document.getElementById('root')).render(
     <ClimaApp/>
     {/* <div>Hola</div> */}
     {/* <appClima></appClima> */}
+    <Footer/>
   </StrictMode>,
 )

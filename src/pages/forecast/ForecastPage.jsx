46ef9f844bd99a1c735077de50e0db8bceaeea92
Laptop <memorosas.00@gmail.com>
2025-04-19 09:30:24 -0600

    se agrega graficacion

--- src/pages/forecast/ForecastPage.jsx
+++ src/pages/forecast/ForecastPage.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { forecast } from "../../hooks/forecast/forecast";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 
 export const ForecastPage = () => {
 
@@ -14,6 +16,31 @@ export const ForecastPage = () => {
         await fetchForecast();
         console.log("se hizo la consulta forecast")
     }
+
+    const [cadenaPronostico, setcadenaPronostico] = useState([]);
+
+    useEffect( () => {
+        
+        if (forecastData?.forecast?.forecastday)
+        {
+            const datoGrafica = forecastData?.forecast?.forecastday.flatMap ( dia =>
+                dia.hour.map(elemento => (
+                    {
+                        tiempo : elemento.time,
+                        temperatura: elemento.temp_c,
+                        // i : index,
+                    }
+                ))
+
+            );
+
+            setcadenaPronostico (datoGrafica);
+        }
+
+        },
+
+    [forecastData]
+    );
 
     return (
         <>
@@ -40,10 +67,10 @@ export const ForecastPage = () => {
                                 </span>
                             </p>
                             <button className="button is-warning is-fullwidth" type="submit">Buscar</button>
-                            {forecastData.forecast.forecastday && (
+                            {forecastData?.forecast?.forecastday && (
 
                                 <>
-                                <ul>
+                                {/* <ul>
                                     {forecastData?.forecast?.forecastday.flatMap(minuto =>
                                         minuto.hour.map((elemento, index) => 
                                         (
@@ -51,7 +78,17 @@ export const ForecastPage = () => {
                                         )
                                     )
                                     )}
-                                </ul>
+                                </ul> */}
+
+                                <ResponsiveContainer width="100%" height={300}>
+                                    <LineChart data={cadenaPronostico}>
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="time" tick={{ fontSize: 10 }} />
+                                        <YAxis dataKey="temperature" unit="°C" />
+                                        <Tooltip />
+                                        <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
+                                    </LineChart>
+                                </ResponsiveContainer>
 
 
 {/*                             // <div>

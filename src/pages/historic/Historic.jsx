82066b4d1d387cf37f91f22dea5ff856db08a202
Guillermo Rosas <guillermorosas@tecnomotum.com>
2025-10-29 15:42:34 -0600

    feat: agrega formulario a pagina historic

--- src/pages/historic/Historic.jsx
+++ src/pages/historic/Historic.jsx
@@ -1,3 +1,4 @@
+import { Form } from "react-router"
 
 
 export const HistoricPage = () => {
@@ -8,6 +9,13 @@ export const HistoricPage = () => {
             <div className="box">
                 <h1 className="title has-text-centered">Historical climate</h1>
 
+                <form>
+                    <p className="control has-icons-left has-icons-right">
+                        <span className="icon is-small is-left"><i className="fas fa-city"></i></span>
+                        <input className="input" type="text" placeholder="Write your city"></input>
+                        <span className="icon is-right"><i className="fas fa-check"></i></span>
+                    </p>
+                </form>
 
             </div>
         </>

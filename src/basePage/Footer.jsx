e4ade058bb8f1803e938504fae33b0fb4b26b697
Guillermo Rosas <memorosas.00@gmail.com>
2025-11-26 13:26:36 -0600

    feat: agrega tabla en footer dommy

--- src/basePage/Footer.jsx
+++ src/basePage/Footer.jsx
@@ -1,8 +1,11 @@
+import { Background } from "victory";
+import "./BasePageStyles.css";
+
 export const Footer = () => {
     return (
         <footer className="footer is-align-content-flex-end">
-          <div className="content has-text-centered">
-            <p>
+          <div className="content is-flex is-justify-content-center">
+            {/* <p>
               <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
               The source code is licensed
               <a href="https://opensource.org/license/mit">MIT</a>. The
@@ -10,7 +13,32 @@ export const Footer = () => {
               <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
                 >CC BY NC SA 4.0</a
               >.
-            </p>
+            </p> */}
+
+            <table className="table is-fullwidth is-transparent">
+                <tbody>
+                    <tr>
+                        <td>Linkedin</td><td>Link</td>
+                        <td className="is-one-fifth"></td>
+                        <td>Redes sociales</td><td>Red</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>Otros proyectos</td>
+                        <td><ul >
+                                <li>Halo</li>
+                                <li>Ususarios App</li>
+                                <li>Clima app</li>
+                            </ul>
+                        </td>
+                        <td className="is-one-fifth"></td>
+                        <td></td>
+                        <td></td>
+                    </tr>                    
+                </tbody>
+
+            </table>
+
           </div>
         </footer>
     );
